7e58410ba7ddfe10fded11136118dc34c3e7f33c
AMikic33 <amikic33@hotmail.com>
2021-01-30 15:12:54 +0000

    detail products added

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='books')
+    path('', views.all_products, name='books'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
